49b6901d4dd22941b1078976346ef85321a56840
edwingarte <edwingarte@gmail.com>
2020-06-21 10:35:27 -0500

    Segundo commit Clase3

--- src/main/java/com/sophos/capacitacion/Nivel1clase2/App.java
+++ src/main/java/com/sophos/capacitacion/Nivel1clase2/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hola Amigo!" );
+        int nombre;
+        int _nombre;
     }
 }
---
+++ src/test/java/com/sophos/capacitacion/Nivel1clase2/VariablesCondicionales.java
@@ -0,0 +1,38 @@
+package com.sophos.capacitacion.Nivel1clase2;
+
+public class VariablesCondicionales {
+	
+	public static void main( String[] args )
+    {
+        byte Variable1 = -50;
+        byte Variable2 = 21;
+        short Variable3 = -30000;
+        short Variable4 = 15000;
+        int Variable5 = -20000000;
+        int Variable6 = 50000000;
+        long Variable7 = -1000000000;
+        long Variable8 = 1000000000;
+        float Variable9 = (float) 23.2155555555555;
+        float Variable10 = (float) 50.2356565656;
+        double Variable11 = 60.255666;
+        double Variable12 = 125.25;
+        boolean Variable13 = false;
+        boolean Variable14 = true;
+        char Variable15 = 'G';
+        char Variable16 = 'B';
+        
+        String Variable17 = "Ejemplo de String";
+        String Variable18 = "Hola a todos";
+        System.out.println(Variable17.contains("S"));		
+		
+		int Edad = 18;
+		if (Edad>17) {
+			System.out.println("Eres mayor de Edad");
+		}else {
+			System.out.println("Eres menor de Edad");
+		}
+	}
+        
+    
+
+}
